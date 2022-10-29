865d1faceeebe74b8e5723c86536d2a395593fd2
cplan85 <carlos_planchart@live.com>
2022-10-30 00:23:55 +0200

    brought in css styling for login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,24 @@ import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
-  {path: 'dashboard', component: DashboardComponent},
+  {path:'', 
+  component: PagesComponent,
+  children: [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'progress', component: ProgressComponent},
+    {path: 'graphics1', component: GraphicsComponent},
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  ]
+
+},
+
+
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'progress', component: ProgressComponent},
-  {path: 'graphics1', component: GraphicsComponent},
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  
   {path: '**', component: NopagefoundComponent},
 ]
 

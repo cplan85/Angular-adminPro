25d89a4ee417b46eaf93e4fd6e32272d46a4a516
cplan85 <carlos_planchart@live.com>
2022-11-06 19:34:04 +0100

    created pages and shared modules

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,31 @@
+import { SharedModule } from './../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { GraphicsComponent } from './graphics/graphics.component';
+import { PagesComponent } from './pages.component';
+import { AppRoutingModule } from '../app-routing.module';
+
+
+@NgModule({
+  declarations: [
+  DashboardComponent,
+  ProgressComponent,
+  GraphicsComponent,
+  PagesComponent
+  ],
+  exports: [
+    DashboardComponent,
+  ProgressComponent,
+  GraphicsComponent,
+  PagesComponent
+  ],
+
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }

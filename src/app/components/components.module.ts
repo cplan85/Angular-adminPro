191dfbeeb405d477dbe5a7e5523b2337885a621b
cplan85 <carlos_planchart@live.com>
2022-11-27 23:55:13 +0100

    incremator component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IncrematorComponent } from './incremator/incremator.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IncrematorComponent
+  ],
+  exports: [
+    IncrematorComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ src/app/components/incremator/incremator.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-incremator',
+  templateUrl: './incremator.component.html',
+  styles: [
+  ]
+})
+export class IncrematorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

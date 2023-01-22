7c6ddeb24b4933f61db7a10c708697d634bba193
cplan85 <carlos_planchart@live.com>
2023-01-22 19:05:39 +0100

    finished input controls on incremator

--- src/app/components/incremator/incremator.component.ts
+++ src/app/components/incremator/incremator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,EventEmitter,Input,  OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incremator',
@@ -6,24 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class IncrematorComponent {
+export class IncrematorComponent implements OnInit {
 
-  progress:number = 40;
-
-  get getPercentage() {
-    return `${this.progress}%`
+  ngOnInit() {
+    this.btnClass= `btn ${this.btnClass}`
   }
+
+  @Input() progress:number = 40;
+  @Input() btnClass: string = 'btn-primary';
+
+  @Output() outputValue: EventEmitter<number> = new EventEmitter();
 
   changeValue(value: number) {
 
     if(this.progress >= 100 && value >= 0) {
+      this.outputValue.emit(100);
       this.progress =100;
     }
 
     if(this.progress <= 0 && value < 0) {
+      this.outputValue.emit(0);
       this.progress =0;
     }
     this.progress = this.progress + value;
+    this.outputValue.emit(this.progress);
+  }
+
+  onChange(newValue: number) {
+
+    if (newValue >= 100) {
+      this.progress = 100;
+    } else if (newValue <= 0) {
+      this.progress = 0;
+    } else {
+      this.progress = newValue;
+    }
+    this.outputValue.emit(this.progress);
   }
 
 }

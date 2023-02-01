75ee68fac43190f3d757a21f1b24a91f714fb60f
cplan85 <carlos_planchart@live.com>
2023-02-01 18:06:14 +0100

    finished checkCurrentTheme logic

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -9,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class AccountSettingsComponent implements OnInit {
 
   public linkTheme = document.querySelector("#theme");
+  public links!: NodeListOf<Element>;
   constructor() { }
 
   ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
+
   }
 
   changeTheme(theme: string) {
@@ -21,6 +25,22 @@ export class AccountSettingsComponent implements OnInit {
 
     this.linkTheme!.setAttribute('href', url);
     localStorage.setItem('theme', url);
+
+    this.checkCurrentTheme()
+  }
+
+  checkCurrentTheme() {
+    this.links.forEach(element => {
+      //working class provides a checkmark to the square
+      element.classList.remove('working');
+      const btnTheme=element.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+
+      if(btnThemeUrl === currentTheme) {
+        element.classList.add('working');
+      }
+    })
   }
 
 }
